c114aae374e43fad84c976d27c91d46c308b053d
Stephen Vlahadamis <stephenvlaha@gmail.com>
2020-08-20 19:35:30 +1000

    Piplet follow inital test

---
+++ Assets/Scripts/Piplet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Piplet : MonoBehaviour
+{
+
+    public float speed = 2;
+    public float stoppingDistace = 3;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistace) {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
