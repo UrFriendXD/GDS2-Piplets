8ee37efe6136c4609dd2953bfb60e52293ac24d4
Stephen Vlahadamis <stephenvlaha@gmail.com>
2020-08-30 18:07:44 +1000

    [PIP02] Build trust is optimized
    Code looked over by Sam and Gandi

--- Assets/Scripts/Piplet.cs
+++ Assets/Scripts/Piplet.cs
@@ -3,25 +3,63 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Piplet : MonoBehaviour
 {
 
-    public float speed = 2;
-    public float stoppingDistace = 3;
-
+    [SerializeField] private float speed = 2;
+    [SerializeField] private float stoppingDistance = 3;
+    public int level; 
+    [SerializeField] private int steps = 0;
+    [SerializeField] private int level2Threshold = 300;
+    [SerializeField] private int level3Threshold = 3000;
+    public GameObject player;
     private Transform target;
+    private bool stepping;
 
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = player.GetComponent<Transform>();
+        level = 1; 
     }
 
 
     void Update()
     {
-        if (Vector2.Distance(transform.position, target.position) > stoppingDistace) {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (player.GroundCheck == true && player.transform.position.y == transform.position.y)
+        {
+            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                if (!stepping)
+                {
+                    StartCoroutine(BuildTrust());
+                }
+            }
+
         }
+        else if (player.GroundCheck == true && player.transform.position.y != transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.transform.position.y,transform.position.z);
+        }
+
+        if (steps > level2Threshold)
+        {
+            level = 2;
+        }
+
+        if (steps > level3Threshold)
+        {
+            level = 3;
+        }
+    }
+
+   private IEnumerator BuildTrust()
+    {
+        steps += 1;
+        stepping = true;
+        yield return new WaitForSeconds(1);
+        stepping = false;
     }
 }
