5a4719bde850eec2b1a0f7f02382a8811d06579e
Sam <sam.huang852@gmail.com>
2020-09-19 01:04:35 +1000

    [DES08] Day tracking & [DES01] soft reset completed
    Game tracks amount of days and years passed in day manager. Each week is 7 days and each year is 14 days. Upon 14 days greenhouse will do a soft reset.

---
+++ Assets/Scripts/Managers/DayManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RoboRyanTron.Unite2017.Events;
+using UnityEngine;
+
+/*
+ * Contains functions relating to days & seasons
+ */
+public class DayManager : MonoBehaviour
+{
+    public int days;
+    public int years;
+
+    [SerializeField] private GameEvent seasonEndEvent;
+    [SerializeField] private SurvivialItemUsed survivalItemUsed;
+    
+    // Change later with service locator pattern
+    [SerializeField] private ItemContainer playerInventory;
+
+    public void DayPassed()
+    {
+        days++;
+        if (WeekCheck())
+        {
+            ConsumeSurvivalItems();
+        }
+    }
+
+    private bool WeekCheck()
+    {
+        if (days == 14)
+        {
+            SeasonEnd();
+        }
+        return days % 7 == 0;
+    }
+
+    private void ConsumeSurvivalItems()
+    {
+        // If player doesn't enough passout else consume items
+        if (!survivalItemUsed.CanCraft(playerInventory))
+        {
+            PassOut();
+        }
+        else
+        {
+            survivalItemUsed.Craft(playerInventory);
+        }
+        Debug.Log("Consume items " + days);
+    }
+
+    // Calls season event event
+    private void SeasonEnd()
+    {
+        seasonEndEvent.Raise();
+        Debug.Log("Season ended " + days);
+        days = 0;
+        years++;
+    }
+
+    private void PassOut()
+    {
+        // just idk end game? pfft dunno
+        Debug.Log("Passed out");
+    }
+}
