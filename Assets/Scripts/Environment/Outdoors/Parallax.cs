97874998c837d1b36a8922c783a7029fd3dc9c41
gandi matar <13221332@student.uts.edu.au>
2020-10-05 19:43:57 +1100

    fixed background

--- Assets/Scripts/Environment/Outdoors/Parallax.cs
+++ Assets/Scripts/Environment/Outdoors/Parallax.cs
@@ -7,6 +7,7 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
+    public float offset;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +22,11 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y);
-        if (temp > startpos + length)
+        if (temp > startpos + (length - offset))
         {
             startpos += length;
         }
-        else if (temp < startpos - length)
+        else if (temp < startpos - (length - offset))
         {
             startpos -= length;
         }
